823786212553020635dc01c7aec79748411ca209
Inf <karn.chanit101@gmail.com>
2017-12-22 00:32:30 +0700

    Update Dbhelper.cs

--- ICT13580031A2/Helpers/Dbhelper.cs
+++ ICT13580031A2/Helpers/Dbhelper.cs
@@ -1,25 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ICT13580031A2.Models;
 using SQLite;
 namespace ICT13580031A2.Helpers
 {
-    public class Dbhelper
-
+ public class DbHelper
     {
         SQLiteConnection db;
-
-        public Dbhelper(string dbPath)
+        public DbHelper(String dbPath)
         {
-			db = new SQLiteConnection(dbPath);
-			db.CreateTable<Product>();
-  		}
-
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<Product>();
+        }
         public int AddProduct(Product product)
         {
-             db.Insert(product);
-            return product.id;
+            db.Insert(product);
+            return product.Id;
+
         }
-
-
+        public List<Product> GetProducts()
+        {
+            return db.Table<Product>().ToList();
+        }
+        public int UpdateProduct(Product product)
+        {
+            return db.Update(product);
+        }
+        public int DeleteProduct(Product product)
+        {
+            return db.Delete(product);
+        }
     }
 }
